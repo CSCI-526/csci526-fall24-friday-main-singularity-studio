fc3e8e054a1a165e38b75d33893316dc03815413
Kasidet Meteeputthi <meteeput@usc.edu>
2024-10-14 13:51:30 -0700

    adjusting the camera rotation issue

--- Assets/Scripts/SceneRotation.cs
+++ Assets/Scripts/SceneRotation.cs
@@ -11,13 +11,24 @@ public class SceneRotation : MonoBehaviour
     private float nextRotateTime = 0.0f;
     private float rotationProgress;
     private bool shouldRotate = true;
+
+
+    public GameObject leftWall;
+    public GameObject rightWall;
+    public GameObject topWall;
+    public GameObject bottomWall;
     
     // Start is called before the first frame update
     void Start()
     {
         nextRotateTime = Time.time + rotationPeriod;
         rotationProgress = 0;
+
+        // UpdateWalls(false);
     }
+
+
+
 
     // Update is called once per frame
     void Update()
@@ -32,9 +43,10 @@ public class SceneRotation : MonoBehaviour
         if(currentTime >= nextRotateTime){
             isRotating = true;
 
-            //check if it currently rotation the scene
+            //check if it currently rotating the scene
             if(rotationProgress < 1 && rotationProgress >= 0){
                 rotationProgress += Time.deltaTime;
+                // UpdateWalls(true);
                 if(isVertical){
                     //rotate the scene from the center of the camera position
                     // https://stackoverflow.com/questions/52737303/in-unity-script-how-to-rotate-and-rotate-to-around-pivot-position reference
@@ -53,11 +65,35 @@ public class SceneRotation : MonoBehaviour
                     transform.rotation = Quaternion.Euler(0, 0, 0.0f);
                 }
 
-                //set the next rotation time
+                // set the next rotation time
                 nextRotateTime = Time.time + rotationPeriod;
                 isVertical = !isVertical;
                 rotationProgress = 0;
                 isRotating = false;
+
+
+                // float currentZRotation = transform.eulerAngles.z;
+
+                // float targetZRotation = isVertical ? 90.0f : 0.0f;
+
+                // if (Mathf.Abs(currentZRotation - targetZRotation) > 0.1f)
+                // {
+                //     float angleDifference = Mathf.LerpAngle(currentZRotation, targetZRotation, Time.deltaTime * 5);
+                //     transform.RotateAround(cameraTransform.position, Vector3.forward, angleDifference - currentZRotation);
+                // }
+                // else
+                // {
+                //     transform.rotation = Quaternion.Euler(0, 0, targetZRotation);
+
+                //     transform.position = cameraTransform.position + (transform.position - cameraTransform.position).normalized * (transform.position - cameraTransform.position).magnitude;
+
+                //     nextRotateTime = Time.time + rotationPeriod;
+                //     isVertical = !isVertical;
+                //     rotationProgress = 0;
+                //     isRotating = false;
+                // }
+
+                // UpdateWalls(false);
             }
         }
     }
@@ -70,5 +106,23 @@ public class SceneRotation : MonoBehaviour
 
     public void setCameraOrientation(){
         isVertical = true;
+    }
+
+    void UpdateWalls(bool isRotating)
+    {
+        if (isRotating)
+        {
+            topWall.SetActive(false);
+            bottomWall.SetActive(false);
+            leftWall.SetActive(false);
+            rightWall.SetActive(false);
+        }
+        else
+        {
+            topWall.SetActive(true);
+            bottomWall.SetActive(true);
+            leftWall.SetActive(true);
+            rightWall.SetActive(true);
+        }
     }
 }
