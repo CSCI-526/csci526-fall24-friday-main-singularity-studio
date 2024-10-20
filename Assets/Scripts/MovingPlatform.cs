6453ab9588a880728b8cae45db34a4c08231174b
Kasidet Meteeputthi <meteeput@usc.edu>
2024-10-19 18:10:39 -0700

    fixing the moving platform code and adding more platform to phase 2

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -9,6 +9,8 @@ public class MovingPlatform : MonoBehaviour
     public float speed = 2f;  
     public float movingDistance = 2;
 
+    public bool moveHorizontally = true;
+
     void Start()
     {
         go = GameObject.Find("GameView");
@@ -17,16 +19,23 @@ public class MovingPlatform : MonoBehaviour
 
     void Update()
     {
-        if (sceneRotation.isVertical)
-        {
-            
-            // transform.localPosition = new Vector3(Mathf.PingPong(Time.time * speed, movingDistance), transform.localPosition.y, transform.localPosition.z);
+        if(moveHorizontally){
             transform.localPosition = new Vector3(Mathf.PingPong(Time.time * speed, movingDistance), transform.localPosition.y, 0);
         }
-        else
-        {
-            // transform.localPosition = new Vector3(transform.localPosition.x, Mathf.PingPong(Time.time * speed, movingDistance), transform.localPosition.z);
+        else{
             transform.localPosition = new Vector3(transform.localPosition.x, Mathf.PingPong(Time.time * speed, movingDistance), 0);
         }
+        
+        // if (sceneRotation.isVertical)
+        // {
+            
+        //     // transform.localPosition = new Vector3(Mathf.PingPong(Time.time * speed, movingDistance), transform.localPosition.y, transform.localPosition.z);
+        //     transform.localPosition = new Vector3(Mathf.PingPong(Time.time * speed, movingDistance), transform.localPosition.y, 0);
+        // }
+        // else
+        // {
+        //     // transform.localPosition = new Vector3(transform.localPosition.x, Mathf.PingPong(Time.time * speed, movingDistance), transform.localPosition.z);
+        //     transform.localPosition = new Vector3(transform.localPosition.x, Mathf.PingPong(Time.time * speed, movingDistance), 0);
+        // }
     }
 }
