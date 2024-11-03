107057e805a38a45eec516bacc745f0685eef7f7
Kasidet Meteeputthi <meteeput@usc.edu>
2024-11-03 00:47:22 -0700

    debugging the stop camera when hit the flag (not done yet)

--- Assets/Scripts/TutorialStopTrigger.cs
+++ Assets/Scripts/TutorialStopTrigger.cs
@@ -7,6 +7,7 @@ public class TutorialStopTrigger : MonoBehaviour
     public CameraMovement cameraMovement;
 
     void OnTriggerEnter2D(Collider2D collision){
+        Debug.Log("in side trigger enter from the tutorial stop");
         if (collision.CompareTag("Flag Trigger")){
             Debug.Log("Stop trigger collide into flag");
             cameraMovement.StopCamera();
