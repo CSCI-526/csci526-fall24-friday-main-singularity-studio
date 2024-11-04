5a8a73a611ab97978ce7e295df2bf4aecde70e14
Guangze Liu <liuguangze2000@163.com>
2024-11-04 13:04:14 -0800

    Pause the camera in tutorial

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEngine;
-
 [RequireComponent(typeof(JumpHandler))]
 [RequireComponent(typeof(SpikeCollision))]
 [RequireComponent(typeof(WallCollision))]
--- Assets/Scripts/TutorialControl.cs
+++ Assets/Scripts/TutorialControl.cs
@@ -5,22 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class TutorialControl : MonoBehaviour
 {
-    public CameraMovement cameraMovement;
-    
-
+    private bool isPaused = false; 
+    public CameraMovement cameraMovement; 
     void Start()
     {
-        Scene currentScene = SceneManager.GetActiveScene();
-        string sceneName = currentScene.name;
-        Debug.Log("start tutorial");
-        if(sceneName == "Tutorial"){
-            Debug.Log("stop the camera");
-            cameraMovement.StopCamera();
+        
+        cameraMovement.StopCamera();
+        Debug.Log("Camera stopped at the start of the tutorial scene");
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Flag Trigger"))
+        {
+            cameraMovement.MoveCamera();
+            Debug.Log("Camera started moving due to Flag Trigger");
+        }
+
+        
+        else if (collision.gameObject.CompareTag("Flag Trigger1") && !isPaused)
+        {
+            StartCoroutine(PauseAndContinueCamera());
         }
     }
-    void OnTriggerEnter2D(Collider2D collision){
-        if (collision.gameObject.CompareTag("Flag Trigger")){
-            cameraMovement.MoveCamera();
-        }
+
+    private IEnumerator PauseAndContinueCamera()
+    {
+        isPaused = true; 
+
+        cameraMovement.StopCamera(); 
+        Debug.Log("Camera paused due to Flag Trigger1");
+
+        yield return new WaitForSeconds(1); 
+
+        cameraMovement.MoveCamera();
+        Debug.Log("Camera resumed moving after pause");
+
+        isPaused = false; 
     }
 }
