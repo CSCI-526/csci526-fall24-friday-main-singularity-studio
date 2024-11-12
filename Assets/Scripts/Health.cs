a9194e3c9540c5539db3297a8d8d1fc46eda58f7
NGOC TA <ngocta0905@gmail.com>
2024-11-11 18:49:27 -0800

    Update progress bar and change UI hearts

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public enum DamageCause
+{
+    Hazard,
+    FortuneHeart
+}
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 3;
+    public int currentHealth;
+
+    public int numOfHearts;
+    public GameObject[] hearts;
+    public GameObject fullHeart;
+    public GameObject emptyHeart;
+
+    //Analytic HeartCollection
+    private HeartTracker currentHeart; 
+    private int previousHealth; 
+    private bool isCollectingHeart = false;
+
+    public delegate void PlayerDiedEventHandler();
+    public event PlayerDiedEventHandler OnPlayerDied;
+
+    private void Start()
+    {
+        if (currentHealth == 0)
+        {
+            ResetHealth();
+            print("We either just started/restarted the game");
+        }
+        UpdateHearts();
+    }
+    public void CollectHeart(HeartTracker heart)
+    {
+        currentHeart = heart;
+        previousHealth = currentHealth;
+        isCollectingHeart = true;
+    }
+
+
+    public void Heal(int healAmount)
+    {
+        // Log healing if a heart was collected
+        if (isCollectingHeart && currentHeart != null)
+        {
+            Debug.Log($"Healing from Collected Heart - ID: {currentHeart.heartID}, Name: {currentHeart.heartName}");
+        }
+        currentHealth += healAmount;
+        if (currentHealth > maxHealth)
+            currentHealth = maxHealth;
+
+        UpdateHearts();
+    }
+
+    public void TakeDamage(int damage, DamageCause cause)
+    {
+        var eventControl = FindObjectOfType<EventControl>();
+        // Log the damage cause for debugging
+        if (cause == DamageCause.FortuneHeart && currentHeart != null)
+        {
+            Debug.Log($"Lost health due to fortune heart - ID: {currentHeart.heartID}, Name: {currentHeart.heartName}");
+        }
+        StartCoroutine(ShowDamage());
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            if (SceneManager.GetActiveScene().name == "Tutorial")
+            {
+                eventControl.ShowGameOverPanel();
+            }
+            currentHealth = 0;
+            OnPlayerDied?.Invoke();
+        }
+        UpdateHearts();
+    }
+
+    IEnumerator ShowDamage()
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    private void UpdateHearts()
+    {
+        //Ensure the player has the maximum of (3) hearts
+        if(maxHealth > numOfHearts){
+            maxHealth = numOfHearts;
+        }
+
+        //Update heart UI upon collision
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < currentHealth){
+                hearts[i].GetComponent<RawImage>().color = new Color(1f, 0.4f, 0.7f); 
+                foreach (Transform child in hearts[i].transform){
+                    child.GetComponent<RawImage>().color = new Color(1f, 0.4f, 0.7f); 
+                }
+            }
+            else{
+                hearts[i].GetComponent<RawImage>().color = new Color(0.5f, 0.5f, 0.5f);
+                foreach (Transform child in hearts[i].transform){
+                    child.GetComponent<RawImage>().color = new Color(0.5f, 0.5f, 0.5f); 
+                }
+            }
+        }
+        // Log heart collection if a heart was collected
+        if (isCollectingHeart && currentHeart != null)
+        {
+            bool gainedHealth = currentHealth > previousHealth;  // Determine if health increased
+            bool lostHealth = currentHealth < previousHealth;    // Determine if health decreased
+
+            AnalyticsManager.TrackHeartCollection(currentHeart.heartID, currentHeart.heartName, gainedHealth, lostHealth);
+
+            // Reset currentHeart and flag after logging
+            currentHeart = null;
+            isCollectingHeart = false;
+        }
+    }
+
+    public void ResetHealth()
+    {
+        currentHealth = maxHealth;
+    }
+}
---
+++ Assets/Scripts/LevelCompletion.cs
@@ -0,0 +1,202 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.VFX;
+using UnityEngine.Analytics;
+
+
+public class LevelCompletion : MonoBehaviour
+{
+    private int currentLevel = 0;
+    private Image ProgressBarImg;
+    private RectTransform rt;
+    private float ProgressBarWidth;
+    public CameraMovement cameraMovement;
+    public SceneRotation sceneRotation;
+    public ParticleSystem confetti1;
+    public ParticleSystem confetti2;
+    public ParticleSystem confetti3;
+    private Vector2 mapPosition;
+    private Vector2 mapScale;
+    private Vector2 topLeftCorner;
+    // private Vector2 bottomRightCorner;
+    public float distanceFromStart;
+    private float mapLength;
+    private Scene currentScene;
+    
+    private float startTime; // Track the game start time
+    private bool isGameStarted = false; // Track if the game has started
+
+    public Health playerHealth;
+
+    private void Start()
+    {
+        ProgressBarImg = GameObject.Find("Progress").GetComponent<Image>();
+        rt = ProgressBarImg.GetComponent<RectTransform>();
+        ProgressBarWidth = rt.rect.width;
+        rt.anchorMin = new Vector2(0, 0.5f);
+        rt.anchorMax = new Vector2(0, 0.5f);
+        rt.sizeDelta = new Vector2(1, rt.sizeDelta.y);
+
+        currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name != "Tutorial"){
+            GameObject player = GameObject.FindWithTag("Player");
+            if (player != null)
+            {
+                playerHealth = player.GetComponent<Health>();
+                playerHealth.OnPlayerDied += HandlePlayerDeath; // Subscribe to the event
+            }
+            else
+            {
+                Debug.LogError("Player object not found. Make sure the player is tagged 'Player' and has a Health component.");
+            }
+            mapLength = 400f;
+            confetti1.Stop();
+            confetti2.Stop();
+            // confetti3.Stop();
+            playerHealth = GameObject.FindWithTag("Player").GetComponent<Health>();
+            playerHealth.OnPlayerDied += HandlePlayerDeath; // Subscribe to the event
+        }else{
+            mapLength = 150f;
+        }
+
+        mapPosition = sceneRotation.transform.position;
+        mapScale = sceneRotation.transform.localScale;
+        topLeftCorner = mapPosition + new Vector2(mapScale.x / 2 - 28, mapScale.y / 2);
+        // bottomRightCorner = mapPosition + new Vector2(mapScale.x / 2, -mapScale.y);
+    }
+
+    private void Update(){
+        // Calculate the distance from the player to the top-left corner
+        print("ball position = " + transform.position + "  top left = " + topLeftCorner);//(25, -9.2)
+        distanceFromStart = Vector2.Distance(topLeftCorner, transform.position);
+        float progress = (mapLength - distanceFromStart)*100 / mapLength;
+        rt.sizeDelta = new Vector2(ProgressBarWidth * (100 - progress)/100, rt.sizeDelta.y);
+    }
+
+    // Method to start tracking playtime
+    public void StartGameTimer()
+    {
+        startTime = Time.time;
+        isGameStarted = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.name == "EndPhase1" && currentScene.name != "Tutorial")
+        {
+            confetti1.Play();
+            // rt.sizeDelta = new Vector2(ProgressBarWidth / 3, rt.sizeDelta.y);
+            Debug.Log("Level 2");
+        }
+        else if (collision.gameObject.name == "EndPhase2" && currentScene.name != "Tutorial")
+        {
+            confetti2.Play();
+            // rt.sizeDelta = new Vector2(2 * ProgressBarWidth / 3, rt.sizeDelta.y);
+            Debug.Log("Level 3");
+        }
+        else if (collision.gameObject.CompareTag("WinTrigger"))
+        {
+            // rt.sizeDelta = new Vector2(ProgressBarWidth, rt.sizeDelta.y);
+            confetti3.Play();
+            Win();
+        }
+        else if (collision.gameObject.CompareTag("LevelTrigger"))
+        {
+            Debug.Log("Leveled up");
+            Destroy(collision.gameObject);
+            currentLevel++;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("WinTrigger")){
+            GameObject cautionTape = GameObject.Find("Caution Tape");
+            if (cautionTape != null){
+                cautionTape.SetActive(false);
+            }
+            GameObject wallDamage = GameObject.Find("Wall Damage Trigger");
+            if (wallDamage != null){
+                wallDamage.SetActive(false);
+            }
+        }
+    }
+    private void HandlePlayerDeath()
+    {
+        Debug.Log("Player has died. Game Over.");
+        UploadDeathData(); // Custom method to upload data
+        var eventControl = FindObjectOfType<EventControl>();
+        if (eventControl != null)
+        {
+            eventControl.ShowGameOverPanel();
+        }
+        else
+        {
+            Debug.LogError("EventControl component is missing.");
+        }
+
+        if (cameraMovement != null)
+        {
+            cameraMovement.StopCamera();
+        }
+        else
+        {
+            Debug.LogError("CameraMovement is missing.");
+        }
+
+        if (sceneRotation != null)
+        {
+            sceneRotation.StopRotation();
+        }
+        else
+        {
+            Debug.LogError("SceneRotation is missing.");
+        }
+    }
+    private void UploadDeathData()
+    {
+        float playTime = Time.time - startTime; // Calculate time spent in level
+        AnalyticsManager.trackProgress(currentLevel, false, playTime); // Upload data indicating game over
+    }
+    private void OnDestroy()
+    {
+        // Unsubscribe from the event to prevent potential memory leaks
+        playerHealth.OnPlayerDied -= HandlePlayerDeath;
+    }
+
+    private void Win()
+    {
+        Debug.Log("Win method called");
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        string sceneName = currentScene.name;
+        if (sceneName == "Tutorial"){
+            cameraMovement.StopCamera();
+            GameObject cautionTape = GameObject.Find("Caution Tape");
+            if (cautionTape != null){
+                cautionTape.SetActive(false);
+            }
+            GameObject wallDamage = GameObject.Find("Wall Damage Trigger");
+            if (wallDamage != null){
+                wallDamage.SetActive(false);
+            }
+            FindObjectOfType<EventControl>().ShowWinTutorial();
+            
+        }
+        else
+        {
+            FindObjectOfType<EventControl>().ShowWinPanel();
+            Debug.Log("Winner Winner Chicken Dinner!");
+            
+            float playTime = Time.time - startTime; // Calculate the play duration
+            AnalyticsManager.trackProgress(currentLevel, true, playTime); // Pass playtime to AnalyticsManager
+            
+            cameraMovement.StopCamera();
+            sceneRotation.StopRotation();
+            isGameStarted = false; // Reset the game state
+        }
+    }
+}
